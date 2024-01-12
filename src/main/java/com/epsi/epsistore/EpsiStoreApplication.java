4c368e705e620a4aca94dfb32820e73625e0c63f
NimZero <contact@nimzero.fr>
2024-01-12 13:22:33 +0100

    Init

---
+++ src/main/java/com/epsi/epsistore/EpsiStoreApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.epsistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsiStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsiStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsi/epsistore/EpsiStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsi.epsistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsiStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
